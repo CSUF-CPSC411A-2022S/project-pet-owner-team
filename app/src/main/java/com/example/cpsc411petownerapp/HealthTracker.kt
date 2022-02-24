0ef3c3b9c7f501e2961c1c2bc4be0275c0e4ca55
Vinh Tran <42789132+kimvinh@users.noreply.github.com>
2022-02-23 21:41:37 -0800

    Update HealthTracker.kt

--- app/src/main/java/com/example/cpsc411petownerapp/HealthTracker.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/HealthTracker.kt
@@ -1,5 +1,15 @@
 package com.example.cpsc411petownerapp
 
-class HealthTracker {
-    // My Code
+class HealthTracker: Pet() {
+    // Declare some possible properties
+    var weight: Int;
+    var timeFeeding: Int;
+    var timeDrinking: Int;
+    var status: String;
+    
+    // Declare som possible methods
+    fun healthStatus() {}
+    fun reminder() {}
+    fun displayMessage() {}
+    fun setTimer() {}
 }
