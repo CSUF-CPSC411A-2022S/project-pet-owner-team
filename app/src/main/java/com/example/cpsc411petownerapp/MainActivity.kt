026b0019a358147ab99fb10895258b83391c052e
Vinh Tran <42789132+kimvinh@users.noreply.github.com>
2022-03-09 21:24:53 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/cpsc411petownerapp/MainActivity.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/MainActivity.kt
@@ -15,23 +15,11 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /**
-         * Remove comments only when working on 6.1 Model 3.
-         * navController refers to our navigation fragment. The setupActionBarWithnavController
-         * method connects our navController to the ActionBar that maintains the "back stack" which
-         * is the succession of fragments that were opened. Pressing the Up button will display the
-         * fragment that opened the current fragment, thereby back tracking its way to the first
-         * fragment.
-         */
         val navController = this.findNavController(R.id.nav_host)
         NavigationUI.setupActionBarWithNavController(this,navController)
 
     }
-    /**
-     * Remove comments only when working on 6.1 Model 3.
-     * The method overrides the default implementation of the Up button so that it uses our
-     * navController.
-     */
+    
     override fun onSupportNavigateUp(): Boolean {
         // Replace nav_host with the name of your nav host fragment in activity_main.xml
         val navController = this.findNavController(R.id.nav_host)
