be636960f72acd78f03348830757a3c5fb022773
Aaliyah <adivinagracia@csu.fullerton.edu>
2022-03-09 22:39:21 -0800

    fragment and navigation v1

--- app/src/main/java/com/example/cpsc411petownerapp/Medication.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/Medication.kt
@@ -5,7 +5,7 @@ abstract class Medication {
     abstract var dosage: Int
     abstract var time: Int
 
-    // this method will take as input the time and will display a reminder message at the appropriate
-    // time
-    abstract fun reminder(time: Int)
+    // this method will take as input the time and medication name, will display a reminder message
+    // at the appropriate time
+    abstract fun reminder(medication: String, time: Int)
 }
