e55d409d6cd79a579bd393b0ba2a77068877659f
Aaliyah <adivinagracia@csu.fullerton.edu>
2022-03-10 16:57:23 -0800

    removed nav and fragment added interface

--- app/src/main/java/com/example/cpsc411petownerapp/MainActivity.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.cpsc411petownerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var clearButton: Button
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val name: EditText = findViewById(R.id.medication)
+            val address: EditText = findViewById(R.id.dosage)
+            val addButton: Button = findViewById(R.id.time)
+            addButton.setOnClickListener {
+                val toast = Toast.makeText(this, "Adding ${name.text} @ ${address.text}.", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+            clearButton = findViewById(R.id.clear)
+            clearButton.setOnClickListener {
+                name.setText("")
+                address.setText("")
+            }
+        }
     }
 }
