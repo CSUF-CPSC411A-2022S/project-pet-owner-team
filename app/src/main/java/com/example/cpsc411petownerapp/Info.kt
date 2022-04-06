c6f30e18ebb3f237baafabdd7e9ac326b5caa8b6
Sara Rutherfurd <sruthfd@gmail.com>
2022-04-06 15:55:59 -0700

    Project check in 4 code

--- app/src/main/java/com/example/cpsc411petownerapp/Info.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/Info.kt
@@ -9,14 +9,13 @@ import android.widget.Toast
 import com.example.cpsc411petownerapp.UserAdapter
 import com.example.cpsc411petownerapp.databinding.InfoBinding
 
+
 class Info : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.info, container, false)
 
         // Inflate and bind the profile.xml layout for the Profile fragment
         val binding = InfoBinding.inflate(layoutInflater)
@@ -24,40 +23,45 @@ class Info : Fragment() {
         var userAdapter = UserAdapter(requireContext())
         binding.userList.adapter = userAdapter
 
-        val str = binding.age.text.toString()
-        val age = str.toInt()
-
-
         //Handle onClick events on the "Add" button.
         binding.add.setOnClickListener {
+            val str = binding.age.text.toString()
+            val age = str.toInt()
 
-            var person = User(
-                age,address = binding.address.text.toString(),sex = binding.sex.text.toString(),
-                name = binding.name.text.toString(), phoneNumber = binding.phoneNumber.text.toString(),
-                petType = binding.petType.text.toString(), petAge = binding.petAge.text.toString(),
-                petName = binding.petName.text.toString()
-            )
+                var person = User(
+                    age,
+                    address = binding.address.text.toString(),
+                    sex = binding.sex.text.toString(),
+                    name = binding.name.text.toString(),
+                    phoneNumber = binding.phoneNumber.text.toString(),
+                    petType = binding.petType.text.toString(),
+                    petAge = binding.petAge.text.toString(),
+                    petName = binding.petName.text.toString()
+                )
 
-            //Toast object to display the appropriate message
-            val toast = Toast.makeText(
-                requireContext(),
-                "Adding ${binding.name.text}${binding.address.text}${binding.sex.text}" +
-                        " ${binding.phoneNumber.text} ${binding.age.text}${binding.petType.text}" +
-                        " ${binding.petAge.text} ${binding.petName.text}" ,
-                Toast.LENGTH_SHORT
-            )
-            toast.show()
+                //Toast object to display the appropriate message
+                val toast = Toast.makeText(
+                    requireContext(),
+                    "Adding ${binding.name.text}${binding.address.text}${binding.sex.text}" +
+                            " ${binding.phoneNumber.text} ${binding.age.text}${binding.petType.text}" +
+                            " ${binding.petAge.text} ${binding.petName.text}",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
 
-            //Add the volunteer to the mutableList and notify the RecyclerView about the change.
-            userAdapter.dataset.add(person)
+                //Add the volunteer to the mutableList and notify the RecyclerView about the change.
+                userAdapter.dataset.add(person)
 
-            // Inform the adapter that we made changes so the visual representation can be updated.
-            userAdapter.notifyDataSetChanged()
+                // Inform the adapter that we made changes so the visual representation can be updated.
+                userAdapter.notifyDataSetChanged()
+
+            }
+        return binding.root
 
         }
 
-        return binding.root
-    }
+
 
 
 }
+
