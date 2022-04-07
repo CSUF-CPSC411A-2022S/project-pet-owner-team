6b0ce744f64fda794a8a3a3e56edb37b095607e7
Sara Rutherfurd <sruthfd@gmail.com>
2022-04-06 21:17:46 -0700

    Project check in 4 code

--- app/src/main/java/com/example/cpsc411petownerapp/Info.kt
+++ app/src/main/java/com/example/cpsc411petownerapp/Info.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.util.Log
 import android.widget.Toast
 import com.example.cpsc411petownerapp.UserAdapter
 import com.example.cpsc411petownerapp.databinding.InfoBinding
 
+// Constant that stores the key for storing and retrieving error message data.
+const val KEY_ERROR = "error"
 
 class Info : Fragment() {
 
@@ -23,10 +26,34 @@ class Info : Fragment() {
         var userAdapter = UserAdapter(requireContext())
         binding.userList.adapter = userAdapter
 
+        // savedInstanceState is only null the first time the View is created.
+        if (savedInstanceState != null) {
+            /**
+             * Restore the error message from the saved data. Bundles behave
+             * like a map where we use the KEY_ERROR constant (equal to "error")
+             * to retrieve the previously stored error message.
+             */
+            binding.errorMsg.setText(savedInstanceState.getString(KEY_ERROR))
+        }
+
         //Handle onClick events on the "Add" button.
         binding.add.setOnClickListener {
-            val str = binding.age.text.toString()
-            val age = str.toInt()
+            var errorMessage = ""
+            //Display an error if there are no inputs
+            if (binding.name.text.length == 0 || binding.address.text.length == 0 ||
+                binding.sex.text.length == 0 || binding.age.text.length == 0 ||
+                binding.petType.text.length == 0 || binding.petAge.text.length == 0 ||
+                binding.petName.text.length == 0
+            ) {
+
+                errorMessage = "Please provide name, address, sex, age, pet type, " +
+                        "pet age, and pet name ";
+
+            } else {
+
+                val str = binding.age.text.toString()
+                val age = str.toInt()
+
 
                 var person = User(
                     age,
@@ -56,12 +83,24 @@ class Info : Fragment() {
                 userAdapter.notifyDataSetChanged()
 
             }
-        return binding.root
+            // Replace the TextView with errorMessage's value, which may or may not be empty.
+            binding.errorMsg.setText(errorMessage)
+        }
+         return binding.root
 
         }
 
-
-
+    /**
+     * Called as the fragment begins to stop. An application may stop when it crashes, it is
+     * forced to close or when the screen rotates and it is recreated.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Save view hierarchy
+        super.onSaveInstanceState(outState)
+        // Bundles act like maps that store values for a given key.
+        // We store the error message String using a constant to avoid mistyping errors.
+        outState.putString(KEY_ERROR, binding.errorMsg.text.toString())
+    }
 
 }
 
